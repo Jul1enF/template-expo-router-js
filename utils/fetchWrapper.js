977570b8b048e5e00c871fbfc97e76fd2dcb7495
Jul1enF <j.furic@gmail.com>
2025-11-05 20:59:01 +0100

    ajout utils/fetchWrapper

---
+++ utils/fetchWrapper.js
@@ -0,0 +1,34 @@
+
+export const fetchWrapper = async (ref, fetchFn, onSuccess = () => { }, setError, setModalVisible) => {
+
+    if (!ref.current) return
+    ref.current = false
+
+    try {
+        const data = await fetchFn()
+
+        if (data?.error) {
+            if (typeof setError === 'function') {
+                setError(data.error)
+                setTimeout(() => setError(''), 5000)
+            }
+        }
+        else if (data?.result) {
+            onSuccess(data)
+        }
+        else {
+            if (typeof setError === 'function') {
+                setError("Erreur : Problème de connexion")
+                setTimeout(() => setError(''), 5000)
+            }
+        }
+    } catch (err) {
+        if (typeof setError === 'function') {
+            setError("Erreur : Problème de connexion")
+            setTimeout(() => setError(''), 5000)
+        }
+    } finally {
+        ref.current = true
+        if (typeof setModalVisible === 'function') setModalVisible(false)
+    }
+}
